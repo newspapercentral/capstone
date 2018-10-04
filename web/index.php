652dae40f18e28abbf19639daf739b91644af2fb
Harry Levin <levin@Harrys-Air.home>
2018-10-04 17:21:12 -0400

    added database access

--- web/index.php
+++ web/index.php
@@ -22,4 +22,37 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+//START MY CODE HERE
+//DB Tutorial
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
+);
+
+//Add new handler to query database
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
+
+// END MY CODE HERE
 $app->run();
