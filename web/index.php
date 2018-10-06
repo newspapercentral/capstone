52fe0507a278efd7e3868c83822e5afe261924c1
Harry Levin <levin@Harrys-Air.home>
2018-10-06 16:02:02 -0400

    trying redirect back to origial route

--- web/index.php
+++ web/index.php
@@ -65,7 +65,8 @@ $app->post('/register', function() use($app) {
   $st->bindValue(1, $username, PDO::PARAM_STR);
   $st->bindValue(2, $password, PDO::PARAM_STR);
   $st->execute();
-  return 'you typed username:' . $username . 'and password' . $password;
+  return $app->redirect('../');
+  //return 'you typed username:' . $username . 'and password' . $password;
   //return $app['twig']->render('index.twig');
 });
 
