bcfa7700d56cb4f9055fe57c3eb41917cb3b37c6
Harry Levin <levin@Harrys-Air.home>
2018-10-06 16:03:46 -0400

    removed bind statements from insert into user table

--- web/index.php
+++ web/index.php
@@ -62,8 +62,8 @@ $app->post('/register', function() use($app) {
   //$password = $request->get('password');  
     
   $st = $app['pdo']->prepare("INSERT INTO user_table (user_nm, password, sec_question, sec_answer) values ('semicolon','semi','semi123', 'semi1234');");
-  $st->bindValue(1, $username, PDO::PARAM_STR);
-  $st->bindValue(2, $password, PDO::PARAM_STR);
+  //$st->bindValue(1, $username, PDO::PARAM_STR);
+  //$st->bindValue(2, $password, PDO::PARAM_STR);
   $st->execute();
   return $app->redirect('../');
   //return 'you typed username:' . $username . 'and password' . $password;
